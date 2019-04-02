f4ede45bf8682edd36a045cacb20b44240df977e
cindymb <cindy.mbonani@cartrack.com>
2019-04-02 08:59:23 +0200

    - SSL fix

--- app/src/main/java/com/cindymb/airportapplication/SSLHelpers.java
+++ app/src/main/java/com/cindymb/airportapplication/SSLHelpers.java
@@ -1,7 +1,8 @@
 package com.cindymb.airportapplication;
 
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
-import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 
 import javax.net.ssl.SSLContext;
@@ -11,33 +12,24 @@ import javax.net.ssl.X509TrustManager;
 
 public class SSLHelpers {
 
-    private X509TrustManager trustManager;
-    private  TrustManager[] trustAllCerts;
-
-    public SSLHelpers() {
-        trustManager=new X509TrustManager() {
-            @Override
-            public void checkClientTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
-
-            }
-
-            @Override
-            public void checkServerTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
-
-            }
-
-            @Override
-            public X509Certificate[] getAcceptedIssuers() {
-                return new X509Certificate[0];
-            }
-        };
-
-
-        trustAllCerts = new TrustManager[]{trustManager};
-    }
-
-    public SSLSocketFactory getTrustAllSocketFactory() {
+    public static SSLSocketFactory getTrustAllSocketFactory() {
         try {
+            TrustManager[] trustAllCerts = new TrustManager[]{
+                    new X509TrustManager() {
+                        public X509Certificate[] getAcceptedIssuers() {
+                            return new X509Certificate[0];
+                        }
+
+                        @Override
+                        public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                        }
+
+                        @Override
+                        public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                        }
+                    }
+            };
+
             SSLContext sslContext = SSLContext.getInstance("SSL");
             sslContext.init(null, trustAllCerts, new SecureRandom());
             return sslContext.getSocketFactory();
@@ -46,9 +38,38 @@ public class SSLHelpers {
         }
     }
 
-    public X509TrustManager getTrustManager(){
-        return  trustManager;
-    }
+    public static SSLSocketFactory getSSLSocketFactory() {
+        try {
+            // Create a trust manager that does not validate certificate chains
+            final TrustManager[] trustAllCerts = new TrustManager[]{
+                    new X509TrustManager() {
+                        @Override
+                        public void checkClientTrusted(X509Certificate[] chain, String authType) {
+                        }
 
+                        @Override
+                        public void checkServerTrusted(X509Certificate[] chain, String authType) {
+                        }
+
+                        @Override
+                        public X509Certificate[] getAcceptedIssuers() {
+                            return new X509Certificate[]{};
+                        }
+                    }
+            };
+
+            // Install the all-trusting trust manager
+            final SSLContext sslContext = SSLContext.getInstance("SSL");
+            sslContext.init(null, trustAllCerts, new SecureRandom());
+            // Create an ssl socket factory with our all-trusting manager
+            final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+
+            return sslSocketFactory;
+        } catch (KeyManagementException | NoSuchAlgorithmException e) {
+            return null;
+        }
+    }
 }
+
+
 
--- app/src/main/java/com/cindymb/airportapplication/di/AppModule.java
+++ app/src/main/java/com/cindymb/airportapplication/di/AppModule.java
@@ -2,28 +2,18 @@ package com.cindymb.airportapplication.di;
 
 import android.app.Application;
 import android.content.Context;
-import android.content.ServiceConnection;
 import android.support.v4.content.LocalBroadcastManager;
-import android.util.Log;
 
 import com.cindymb.airportapplication.BuildConfig;
-import com.cindymb.airportapplication.SSLHelpers;
 import com.cindymb.airportapplication.model.NearbyAirportRequestModel;
 import com.cindymb.airportapplication.services.ApiService;
 import com.cindymb.airportapplication.utils.LoggingHelper;
 import com.facebook.stetho.okhttp3.StethoInterceptor;
 
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import java.security.cert.X509Certificate;
 import java.util.concurrent.TimeUnit;
 
 import javax.inject.Singleton;
-import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSocketFactory;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
 
 import dagger.Module;
 import dagger.Provides;
@@ -31,6 +21,9 @@ import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+
+import static com.cindymb.airportapplication.SSLHelpers.getSSLSocketFactory;
+import static com.cindymb.airportapplication.SSLHelpers.getTrustAllSocketFactory;
 
 @Module(includes = ViewModelModule.class)
 public class AppModule {
@@ -65,87 +58,31 @@ public class AppModule {
     @Singleton
     @Provides
     OkHttpClient providesOkHttpClient() {
-        SSLHelpers sslHelpers = new SSLHelpers();
-        SSLSocketFactory sslSocketFactory = null;
-        OkHttpClient client;
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
 
         try {
-            sslSocketFactory = getSSLSocketFactory();
+            SSLSocketFactory sslSocketFactory = getSSLSocketFactory();
             if (sslSocketFactory == null) {
                 sslSocketFactory = getTrustAllSocketFactory();
             }
+
+            HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+            interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+            httpClient.addInterceptor(interceptor);
+            httpClient.sslSocketFactory(sslSocketFactory);
+
+            if (BuildConfig.DEBUG) {
+                httpClient.addNetworkInterceptor(new StethoInterceptor());
+                httpClient.addNetworkInterceptor(new StethoInterceptor());
+            }
+            httpClient.connectTimeout(30, TimeUnit.SECONDS);
         } catch (Exception e) {
             LoggingHelper.information(AppModule.class, e.getMessage());
         }
-        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
-        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
-        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-        httpClient.addInterceptor(interceptor);
-        httpClient.sslSocketFactory(sslSocketFactory);
-        if (BuildConfig.DEBUG) {
-            httpClient.addNetworkInterceptor(new StethoInterceptor());
-        }
-        httpClient.connectTimeout(30, TimeUnit.SECONDS);
+
         return httpClient.build();
     }
 
-
-    public static SSLSocketFactory getTrustAllSocketFactory() {
-        try {
-            TrustManager[] trustAllCerts = new TrustManager[]{
-                    new X509TrustManager() {
-                        public X509Certificate[] getAcceptedIssuers() {
-                            return new X509Certificate[0];
-                        }
-
-                        @Override
-                        public void checkClientTrusted(X509Certificate[] certs, String authType) {
-                        }
-
-                        @Override
-                        public void checkServerTrusted(X509Certificate[] certs, String authType) {
-                        }
-                    }
-            };
-
-            SSLContext sslContext = SSLContext.getInstance("SSL");
-            sslContext.init(null, trustAllCerts, new SecureRandom());
-            return sslContext.getSocketFactory();
-        } catch (Exception e) {
-            return null;
-        }
-    }
-    private static SSLSocketFactory getSSLSocketFactory() {
-        try {
-            // Create a trust manager that does not validate certificate chains
-            final TrustManager[] trustAllCerts = new TrustManager[]{
-                    new X509TrustManager() {
-                        @Override
-                        public void checkClientTrusted(X509Certificate[] chain, String authType) {
-                        }
-
-                        @Override
-                        public void checkServerTrusted(X509Certificate[] chain, String authType) {
-                        }
-
-                        @Override
-                        public X509Certificate[] getAcceptedIssuers() {
-                            return new X509Certificate[]{};
-                        }
-                    }
-            };
-
-            // Install the all-trusting trust manager
-            final SSLContext sslContext = SSLContext.getInstance("SSL");
-            sslContext.init(null, trustAllCerts, new SecureRandom());
-            // Create an ssl socket factory with our all-trusting manager
-            final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
-
-            return sslSocketFactory;
-        } catch (KeyManagementException | NoSuchAlgorithmException e) {
-            return null;
-        }
-    }
 
     /**
      * Provides the Application Context
